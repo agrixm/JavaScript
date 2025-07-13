4256f9779165459ed91035385bc0d47ec0689ea5
agrixm <sagrim510@gmail.com>
2025-07-13 23:23:05 +0530

    Day-6{invokedFunction}

---
+++ invokedFunction.js
@@ -0,0 +1,12 @@
+(function chai() {
+    console.log("This is the invoked function context.");
+})()
+
+()() // This is an immediately invoked function expression (IIFE)
+
+
+
+
+
+//what is IIFE?
+//IIFE stands for Immediately Invoked Function Expression. It is a JavaScript function that runs as soon as it is defined. It is a common design pattern used to create a new scope and avoid polluting the global namespace.
